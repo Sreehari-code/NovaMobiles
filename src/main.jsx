2e1c94301c9b72f1cb4fb9f823bb717b1ca3bae3
Sreehari-Code <sreeharis0022@gmail.com>
2025-10-20 12:06:37 +0530

    NovaMobiles

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import AppLayout from './Pages/AppLayout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/NovaMobiles/">
    <AppLayout/>
     </BrowserRouter>
   </StrictMode>,
